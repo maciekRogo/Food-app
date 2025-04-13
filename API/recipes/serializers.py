0e02eb03f976162599897f6aaa20196c4243369c
VICMOSK10 <VICMOSK10@edu.gdansk.pl>
2025-04-13 23:21:29 +0200

    dodano endpoint

--- API/recipes/serializers.py
+++ API/recipes/serializers.py
@@ -1,22 +1,26 @@
 from rest_framework import serializers
-from .models import Recipe, Ingredient, RecipeIngredient
-
+from .models import Recipe, Ingredient
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ['id', 'name']
 
-
 class RecipeSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(many=True)
+    ingredients = serializers.ListField(
+        child=serializers.CharField(), write_only=True
+    )
 
     class Meta:
         model = Recipe
         fields = ['id', 'name_of_the_dish', 'img_url', 'diet_type', 'ingredients']
 
+    def create(self, validated_data):
+        ingredient_names = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(**validated_data)
 
-class RecipeIngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RecipeIngredient
-        fields = ['recipe', 'ingredient']
+        for name in ingredient_names:
+            ingredient, _ = Ingredient.objects.get_or_create(name=name)
+            recipe.ingredients.add(ingredient)
+
+        return recipe
