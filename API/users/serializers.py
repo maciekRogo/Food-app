346a4462419af18169bf0f59ecc7a83373a68bbc
obieuz <robizaj2006@gmail.com>
2025-06-08 10:37:09 +0200

    dodalem endpointy do logowania i rejestracji

--- API/users/serializers.py
+++ API/users/serializers.py
@@ -26,3 +26,4 @@ class LoginSerializer(serializers.Serializer):
         if user and user.is_active:
             return user
         raise serializers.ValidationError("Niepoprawne dane logowania")
+
--- API/users/urls.py
+++ API/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import *
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('login_via_request/', login_via_request, name='login_via_request'),
+    path('register_via_request/', register_via_request, name='register_via_request'),
 ]
