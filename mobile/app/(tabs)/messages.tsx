feb13bdc6c3c69591e2eb7defbf349f5a2793dbe
obieuz <robizaj2006@gmail.com>
2025-06-08 16:50:57 +0200

    dodalem interakcje z chatgpt

--- mobile/app/(tabs)/messages.tsx
+++ mobile/app/(tabs)/messages.tsx
@@ -1,9 +1,11 @@
-import {View, Text, StyleSheet, Dimensions, Image, ScrollView} from "react-native";
+import {View, Text, StyleSheet, Dimensions, Image, ScrollView, TouchableOpacity} from "react-native";
 import {useStore} from "@/constants/store";
+import {useRouter} from "expo-router";
 const { width, height } = Dimensions.get('window');
 
 export default function MessagesScreen() {
     const { recipes,interactedRecipes } = useStore();
+    let router = useRouter();
 
     let availableRecipes = recipes.filter(recipe => {
         let interaction = interactedRecipes.find(interaction => interaction.recipeId === recipe.id);
@@ -24,9 +26,16 @@ export default function MessagesScreen() {
                 <Text>Messages Screen</Text>
                 {availableRecipes.length > 0 ? (
                     availableRecipes.map((recipe) => (
-                        <View key={recipe.id} style={styles.row}>
-                            <Image source={{uri: recipe.img_url}} style={{width:64,height:64}}/>
-                            <Text>{recipe.title}</Text>
+                        <View key={recipe.id}>
+                            <TouchableOpacity
+                                style={styles.row}
+                                onPress={()=>{
+                                    router.push(`/messages/${recipe.id}`)
+                                }}
+                            >
+                                <Image source={{uri: recipe.img_url}} style={{width:64,height:64}}/>
+                                <Text>{recipe.title}</Text>
+                            </TouchableOpacity>
                         </View>
                     ))): <View>Działaj kociaku albo dzisiaj nic nie jesz</View>}
             </View>
