eefb100953ac0b66613380d62c37413ef5a3f928
obieuz <robizaj2006@gmail.com>
2025-06-08 15:23:11 +0200

    przepisy pojawiaja sie w messages

--- mobile/app/(tabs)/messages.tsx
+++ mobile/app/(tabs)/messages.tsx
@@ -1,9 +1,53 @@
-import {View,Text} from "react-native";
+import {View, Text, StyleSheet, Dimensions, Image, ScrollView} from "react-native";
+import {useStore} from "@/constants/store";
+const { width, height } = Dimensions.get('window');
 
 export default function MessagesScreen() {
+    const { recipes,interactedRecipes } = useStore();
+
+    let availableRecipes = recipes.filter(recipe => {
+        let interaction = interactedRecipes.find(interaction => interaction.recipeId === recipe.id);
+        if (interaction){
+            return interaction.action !== 'dislike';
+        }
+        return false;
+    });
+
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Messages Screen</Text>
-        </View>
+        <ScrollView
+            contentContainerStyle={{ flexGrow: 1 }}
+        >
+            <View
+                style={styles.container}
+            >
+                <Text>Messages Screen</Text>
+                {availableRecipes.length > 0 ? (
+                    availableRecipes.map((recipe) => (
+                        <View key={recipe.id} style={styles.row}>
+                            <Image source={{uri: recipe.img_url}} style={{width:64,height:64}}/>
+                            <Text>{recipe.title}</Text>
+                        </View>
+                    ))): <View>Działaj kociaku albo dzisiaj nic nie jesz</View>}
+            </View>
+        </ScrollView>
     );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+        paddingTop: 50,
+        paddingBottom: 80,
+    },
+    row:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: width - 40,
+        marginBottom: 20,
+    }
+});
